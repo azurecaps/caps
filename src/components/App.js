6529c3d2302aa355eaf345cbee56063fa9aaf0d9
Hamza Alaoui <hamzarj96@gmail.com>
2019-08-25 11:00:05 +0200

    progress

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Capsule from "./Capsule/Capsule";
+
+function App() {
+    return (
+        <div className="App">
+            <Capsule/>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from './store/createStore';
+import {Provider} from 'react-redux';
+import {Route} from 'react-router';
+import {ConnectedRouter} from "react-router-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Create Redux store
+const store = createStore();
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+// Render App
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+        {/*<Route component={App} />*/}
+
+    </Provider>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
